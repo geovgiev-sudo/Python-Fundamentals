f20497da4b0fc654908e4e6dfb5c120f350b8a34
Georgi Georgiev <geovgiev@gmail.com>
2026-02-16 19:09:45 +0200

    updates

--- 3_Word_Reverse.py
+++ 3_Word_Reverse.py
@@ -1,5 +1,11 @@
-word = input()
+from operator import index
 
-reversed_word = word[::-1]
+word = input()
+reversed_word = ""
+
+for i in range(len(word) -1, -1, -1):
+    reversed_word += word[i]
+    print(word[i], end="")
+#reversed_word = word[::-1]
 
 print(reversed_word)
