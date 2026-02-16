476380bfc3464ab380e85f5ca82b2453399cd143
Georgi Georgiev <geovgiev@gmail.com>
2026-02-16 21:51:48 +0200

    Delete 02-Data-Types-and-Variables-Exercise directory

--- 02-Data-Types-and-Variables-Exercise/4_Sum_of_Chars.py
+++
@@ -1,7 +0,0 @@
-number_of_characters = int(input())
-total_sum = 0
-for char in range(1, number_of_characters + 1):
-    letter = input()
-    total_sum += ord(letter)
-
-print(f'The sum equals: {total_sum}')
--- 02-Data-Types-and-Variables-Exercise/5_Print_Part_of_the_ASCII_Table.py
+++
@@ -1,9 +0,0 @@
-starting_index = int(input())
-ending_index = int(input())
-
-for char in range(starting_index, ending_index + 1):
-    symbol = chr(char)
-    if char == ending_index:
-        print(chr(char))
-    else:
-        print(f'{symbol} ', end='')
