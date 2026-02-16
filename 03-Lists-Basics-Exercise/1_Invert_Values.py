339960f0956fbaffaef07e857d1774ef68e386db
Georgi Georgiev <geovgiev@gmail.com>
2026-02-16 21:52:09 +0200

    Delete 03-Lists-Basics-Exercise directory

--- 03-Lists-Basics-Exercise/1_Invert_Values.py
+++
@@ -1,7 +0,0 @@
-single_string = input().split()
-opposite_list = []
-
-for num in single_string:
-    opposite_list.append(-int(num))
-
-print(opposite_list)
--- 03-Lists-Basics-Exercise/2_Multiples_List.py
+++
@@ -1,9 +0,0 @@
-factor = int(input())
-count = int(input())
-
-lst = []
-
-for i in range(1, count + 1):
-    lst.append(factor * i)
-
-print(lst)
