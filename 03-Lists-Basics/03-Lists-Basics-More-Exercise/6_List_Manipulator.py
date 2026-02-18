0646e6323b966b38a39483ec8808f63ab7a3c181
Georgi Georgiev <geovgiev@gmail.com>
2026-02-19 01:37:41 +0200

    Added new solutions to tasks

--- 03-Lists-Basics/03-Lists-Basics-More-Exercise/6_List_Manipulator.py
+++ 03-Lists-Basics/03-Lists-Basics-More-Exercise/6_List_Manipulator.py
@@ -1,3 +1,125 @@
+integers = list(map(int, input().split()))
+command = input()
+
+while command != "end":
+    command = command.split()
+    action = command[0]
+
+    if action == "exchange":
+        index = int(command[1])
+        if 0 <= index < len(integers):
+            integers = integers[index + 1:] + integers[:index + 1]
+        else:
+            print("Invalid index")
+
+    elif action == "max" or action == "min":
+        odd_even = command[1]
+        indices = []
+        if odd_even == "even":
+            indices = [i for i in range(len(integers)) if integers[i] % 2 == 0]
+        elif odd_even == "odd":
+            indices = [i for i in range(len(integers)) if integers[i] % 2 != 0]
+
+        if not indices:
+            print("No matches")
+        else:
+            min_max = 0
+            if action == "max":
+                min_max = max([integers[i] for i in indices])
+            elif action == "min":
+                min_max = min([integers[i] for i in indices])
+            rightmost_index = [i for i in indices if integers[i] == min_max][-1]
+            print(rightmost_index)
+
+    elif action == "first" or action == "last":
+        count = int(command[1])
+        odd_even = command[2]
+        filtered = []
+        if 0 < count <= len(integers):
+            if odd_even == "even":
+                filtered = [x for x in integers if x % 2 == 0]
+            elif odd_even == "odd":
+                filtered = [x for x in integers if x % 2 != 0]
+            if action == "first":
+                print(filtered[:count])
+            elif action == "last":
+                print(filtered[-count:])
+        else:
+            print("Invalid count")
+    command = input()
+
+print(integers)
+
+
+
+
+
+# integers = list(map(int, input().split()))
+# command = input()
+#
+# while command != "end":
+#     command = command.split()
+#     action = command[0]
+#
+#     even_numbers = [even for even in integers if even % 2 == 0]
+#     odd_numbers = [odd for odd in integers if odd % 2 != 0]
+#
+#     if action == "exchange":
+#         index = int(command[1])
+#         if 0 <= index < len(integers):
+#             integers = integers[index + 1:] + integers[:index + 1]
+#         else:
+#             print("Invalid index")
+#
+#     elif action == "max":
+#         odd_even = command[1]
+#         if odd_even == "even" and even_numbers:
+#             print((len(integers) - integers[::-1].index(max(even_numbers)) - 1))
+#         elif odd_even == "odd" and odd_numbers:
+#             print((len(integers) - integers[::-1].index(max(odd_numbers)) - 1))
+#         else:
+#             print("No matches")
+#
+#     elif action == "min":
+#         odd_even = command[1]
+#         if odd_even == "even" and even_numbers:
+#             print((len(integers) - integers[::-1].index(min(even_numbers)) - 1))
+#         elif odd_even == "odd" and odd_numbers:
+#             print((len(integers) - integers[::-1].index(min(odd_numbers)) - 1))
+#         else:
+#             print("No matches")
+#
+#     elif action == "first":
+#         count = int(command[1])
+#         odd_even = command[2]
+#         if 0 < count <= len(integers):
+#             if odd_even == "even":
+#                 print(even_numbers[0:count])
+#             elif odd_even == "odd":
+#                 print(odd_numbers[0:count])
+#         else:
+#             print("Invalid count")
+#
+#     elif action == "last":
+#         count = int(command[1])
+#         odd_even = command[2]
+#         if 0 < count <= len(integers):
+#             if odd_even == "even":
+#                 print(even_numbers[-count:])
+#             elif odd_even == "odd":
+#                 print(odd_numbers[-count:])
+#         else:
+#             print("Invalid count")
+#
+#     command = input()
+#
+# print(integers)
+#
+
+
+
+
+
 # # Initial list setup [cite: 27]
 # numbers = [int(x) for x in input().split()]
 #
