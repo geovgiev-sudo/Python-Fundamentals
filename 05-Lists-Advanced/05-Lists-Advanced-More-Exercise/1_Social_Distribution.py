87df6774a1808be384bbd9d454344cabd2b5115c
Georgi Georgiev <geovgiev@gmail.com>
2026-02-18 16:06:27 +0200

    Added new solutions to tasks

--- 05-Lists-Advanced/05-Lists-Advanced-More-Exercise/1_Social_Distribution.py
+++ 05-Lists-Advanced/05-Lists-Advanced-More-Exercise/1_Social_Distribution.py
@@ -1,2 +1,27 @@
-population = input().split(", ")
+population = [int(i) for i in input().split(", ")]
 minimum_wealth = int(input())
+
+population = list(map(int, input().split(", ")))
+min_wealth = int(input())
+
+
+if sum(population) < min_wealth * len(population):
+    print("No equal distribution possible")
+else:
+    for i in range(len(population)):
+        if population[i] < min_wealth:
+            need = min_wealth - population[i]
+
+            while need > 0:
+                richest_idx = population.index(max(population))
+                available = population[richest_idx] - min_wealth
+
+                if available <= 0:
+                    break
+
+                give = min(need, available)
+                population[richest_idx] -= give
+                population[i] += give
+                need -= give
+
+    print(population)
