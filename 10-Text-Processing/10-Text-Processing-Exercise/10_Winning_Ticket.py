c6bddd400bd6dab8dabea4bf2dd4f3ef1f53c7a8
Georgi Georgiev <geovgiev@gmail.com>
2026-03-13 17:00:18 +0200

    Added new solutions to tasks

--- 10-Text-Processing/10-Text-Processing-Exercise/10_Winning_Ticket.py
+++ 10-Text-Processing/10-Text-Processing-Exercise/10_Winning_Ticket.py
@@ -1,58 +1,101 @@
-def check_ticket(ticket: str) -> str:
-    if len(ticket) != 20:
-        return "invalid ticket"
-    winning_symbols = ('@', '#', '$', '^')
-    left_part = ticket[:10]
-    right_part = ticket[10:]
-    for match_symbol in winning_symbols:
-        for uninterrupted_match_length in range(10, 5, -1):
-            match_symbol_repetition = uninterrupted_match_length * match_symbol
-            # Case where we have a winning ticket
-            if match_symbol_repetition in left_part and match_symbol_repetition in right_part:
-                # Case Jackpot
-                if uninterrupted_match_length == 10:
-                    return f'ticket "{ticket}" - {uninterrupted_match_length}{match_symbol} Jackpot!'
-                # Case just a winning ticket
-                return f'ticket "{ticket}" - {uninterrupted_match_length}{match_symbol}'
-    return f'ticket "{ticket}" - no match'
+# def check_ticket(ticket: str) -> str:
+#     if len(ticket) != 20:
+#         return "invalid ticket"
+#     winning_symbols = ('@', '#', '$', '^')
+#     left_part = ticket[:10]
+#     right_part = ticket[10:]
+#     for match_symbol in winning_symbols:
+#         for uninterrupted_match_length in range(10, 5, -1):
+#             match_symbol_repetition = uninterrupted_match_length * match_symbol
+#             # Case where we have a winning ticket
+#             if match_symbol_repetition in left_part and match_symbol_repetition in right_part:
+#                 # Case Jackpot
+#                 if uninterrupted_match_length == 10:
+#                     return f'ticket "{ticket}" - {uninterrupted_match_length}{match_symbol} Jackpot!'
+#                 # Case just a winning ticket
+#                 return f'ticket "{ticket}" - {uninterrupted_match_length}{match_symbol}'
+#     return f'ticket "{ticket}" - no match'
+#
+#
+# tickets = [ticket.strip() for ticket in input().split(", ")]
+# for current_ticket in tickets:
+#     print(check_ticket(current_ticket))
+#
+#
+#
+# # ver 2
+#
+# def find_match(part):
+#     for symbol in "@#$^":
+#         for count in range(10, 5, -1):
+#             if symbol * count in part:
+#                 return symbol, count
+#     return "", 0
+#
+#
+# tickets = input().split(", ")
+# for ticket in tickets:
+#     ticket = ticket.strip()
+#
+#     if len(ticket) != 20:
+#         print("invalid ticket")
+#         continue
+#
+#     left = ticket[:10]
+#     right = ticket[10:]
+#
+#     left_symbol, left_count = find_match(left)
+#     right_symbol, right_count = find_match(right)
+#
+#     if left_symbol == right_symbol and left_symbol != "":
+#         match_count = min(left_count, right_count)
+#
+#         if match_count == 10:
+#             print(f'ticket "{ticket}" - {match_count}{left_symbol} Jackpot!')
+#         else:
+#             print(f'ticket "{ticket}" - {match_count}{left_symbol}')
+#     else:
+#         print(f'ticket "{ticket}" - no match')
 
 
-tickets = [ticket.strip() for ticket in input().split(", ")]
-for current_ticket in tickets:
-    print(check_ticket(current_ticket))
+# ver3 Ines Ivanova 2021
 
 
-
-# ver 2
-
-def find_match(part):
-    for symbol in "@#$^":
-        for count in range(10, 5, -1):
-            if symbol * count in part:
-                return symbol, count
-    return "", 0
+winning_symbols = ["@", "#", "$", "^"]
 
 
-tickets = input().split(", ")
+def is_jackpot(ticket):
+    for winning_symbol in winning_symbols:
+        if winning_symbol in ticket:
+            if ticket.count(winning_symbol) == 20:
+                print(f"ticket \"{ticket}\" - 10{winning_symbol} Jackpot!")
+                return True
+    return False
+
+
+def is_winning_ticket(ticket):
+    left_half = ticket[:10]
+    right_half = ticket[10:]
+    for winning_symbol in winning_symbols:
+        if winning_symbol * 6 in left_half and winning_symbol * 6 in right_half:
+            left_symbols_count = left_half.count(winning_symbol)
+            right_symbols_count = right_half.count(winning_symbol)
+            match_result = min(left_symbols_count, right_symbols_count)
+            print(f"ticket \"{ticket}\" - {match_result}{winning_symbol}")
+            return True
+    return False
+
+
+tickets = [el.strip() for el in input().split(", ")]
+
 for ticket in tickets:
-    ticket = ticket.strip()
-
-    if len(ticket) != 20:
-        print("invalid ticket")
+    if not len(ticket) == 20:
+        print(f"invalid ticket")
+        continue
+    if is_jackpot(ticket):
         continue
 
-    left = ticket[:10]
-    right = ticket[10:]
+    if is_winning_ticket(ticket):
+        continue
 
-    left_symbol, left_count = find_match(left)
-    right_symbol, right_count = find_match(right)
-
-    if left_symbol == right_symbol and left_symbol != "":
-        match_count = min(left_count, right_count)
-
-        if match_count == 10:
-            print(f'ticket "{ticket}" - {match_count}{left_symbol} Jackpot!')
-        else:
-            print(f'ticket "{ticket}" - {match_count}{left_symbol}')
-    else:
-        print(f'ticket "{ticket}" - no match')
+    print(f"ticket \"{ticket}\" - no match")
--- 10-Text-Processing/10-Text-Processing-Exercise/9_Rage_Quit.py
+++ 10-Text-Processing/10-Text-Processing-Exercise/9_Rage_Quit.py
@@ -1,47 +1,76 @@
-text = input()
-rage_message = ""
-sub_string = ""
-repetitions = ""
+# text = input()
+# rage_message = ""
+# sub_string = ""
+# repetitions = ""
+#
+# for index in range(len(text)):
+#     # Case where we have non-numeric symbol
+#     if not text[index].isdigit():
+#         sub_string += text[index].upper()
+#         # Case where we have digit
+#     else:  # elif text[index].isdigit()
+#         repetitions += text[index]
+#         if index + 1 < len(text):
+#             if text[index + 1].isdigit():
+#                 repetitions += text[index + 1]
+#         rage_message += sub_string * int(repetitions)
+#         sub_string = ""
+#         repetitions = ""
+#
+# print(f"Unique symbols used: {len(set(rage_message))}")
+# print(rage_message)
+#
+#
+# # ver 2
+#
+# string_sequence = input()
+#
+# result = ""
+# current_string = ""
+# current_number = ""
+#
+# for char in string_sequence:
+#     if not char.isdigit():
+#         if current_number:
+#             result += current_string * int(current_number)
+#             current_string = ""
+#             current_number = ""
+#         current_string += char
+#     else:
+#         current_number += char
+#
+# result += current_string * int(current_number)
+#
+# result = result.upper()
+#
+# print(f"Unique symbols used: {len(set(result))}")
+# print(result)
 
-for index in range(len(text)):
-    # Case where we have non-numeric symbol
-    if not text[index].isdigit():
-        sub_string += text[index].upper()
-        # Case where we have digit
-    else:  # elif text[index].isdigit()
-        repetitions += text[index]
-        if index + 1 < len(text):
-            if text[index + 1].isdigit():
-                repetitions += text[index + 1]
-        rage_message += sub_string * int(repetitions)
-        sub_string = ""
-        repetitions = ""
 
-print(f"Unique symbols used: {len(set(rage_message))}")
-print(rage_message)
+# ver 3 Ines Ivanova 2021
 
+data = input()
 
-# ver 2
-
-string_sequence = input()
-
-result = ""
+index = 0
 current_string = ""
-current_number = ""
+final_result = ""
 
-for char in string_sequence:
-    if not char.isdigit():
-        if current_number:
-            result += current_string * int(current_number)
-            current_string = ""
-            current_number = ""
-        current_string += char
+while index < len(data):
+
+    if not data[index].isdigit():
+        current_string += data[index]
+        index += 1
     else:
-        current_number += char
+        current_number = ""
+        while data[index].isdigit():
+            current_number += data[index]
+            index += 1
+            if index == len(data):
+                break
+        current_number = int(current_number)
+        output = current_string.upper() * current_number
+        final_result += output
+        current_string = ""
 
-result += current_string * int(current_number)
-
-result = result.upper()
-
-print(f"Unique symbols used: {len(set(result))}")
-print(result)
+print(f"Unique symbols used: {len(set(final_result))}")
+print(final_result)
